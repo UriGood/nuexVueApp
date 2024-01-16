35a60836a5524725d9888f0882002022eb8dd7ff
uriel.martinez <uriel.martinez@inateltech.com>
2024-01-16 01:25:31 -0600

    se corrigen rutas

---
+++ pages/dashboard.vue
@@ -0,0 +1,35 @@
+<script lang="ts" setup>
+  import { StatusCards } from '../interfaces/status.cards.type'
+</script>
+<template>
+  <NavBarComponent />
+  <div class="container mx-auto mt-20">
+    <div class="mx-4 lg:mx-0">
+      <DhbHeaderComponent />
+    </div>
+    <div class="grid grid-cols-1 sm:grid-cols-4  gap-4 mx-4 lg:mx-0">
+          <DhbCardComponent 
+          :typeEvaluation='StatusCards.ApprovedEvaluation' 
+          :percent-one="{ percent:'8%', detail:'con cita'}" 
+          :percent-two="{ percent:'13%', detail:'sin cita'}" 
+          />
+
+          <DhbCardComponent 
+          :typeEvaluation='StatusCards.NotAcceptOffer' 
+          :percent-one="{ percent:'19%'}" />
+
+          <DhbCardComponent 
+          :typeEvaluation='StatusCards.IncompleteEvaluation' 
+          :percent-one="{ percent:'20%'}" />
+
+          <DhbCardComponent 
+          :typeEvaluation='StatusCards.NoFitTheProfile' 
+          :percent-one="{ percent:'40%'}" />
+    </div>
+    <div class="grid grid-cols-1 gap-4  mx-4 lg:mx-0">
+        <div class="">
+          <DhbTableComponent />
+        </div>
+    </div>
+  </div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,35 +1,43 @@
-<script lang="ts" setup>
-  import { StatusCards } from '../interfaces/status.cards.type'
-</script>
+<script setup lang="ts"></script>
+
 <template>
-  <NavBarComponent />
-  <div class="container mx-auto mt-20">
-    <div class="mx-4 lg:mx-0">
-      <DhbHeaderComponent />
-    </div>
-    <div class="grid grid-cols-1 sm:grid-cols-4  gap-4 mx-4 lg:mx-0">
-          <DhbCardComponent 
-          :typeEvaluation='StatusCards.ApprovedEvaluation' 
-          :percent-one="{ percent:'8%', detail:'con cita'}" 
-          :percent-two="{ percent:'13%', detail:'sin cita'}" 
-          />
+  <div
+    class="container mx-auto justify-center items-center flex"
+    style="height: 100vh"
+  >
+    <div
+      class="login text-center bg-white py-5 px-8 rounded-lg drop-shadow w-full sm:w-1/3 mx-3"
+    >
+      <h1 class="text-2xl font-bold">Bienvenido</h1>
+      <p class="text-xs my-2">Ingresa tu correo electronico y tu contraseña</p>
 
-          <DhbCardComponent 
-          :typeEvaluation='StatusCards.NotAcceptOffer' 
-          :percent-one="{ percent:'19%'}" />
+      <label class="block my-5">
+        <span class="block text-left text-sm font-medium text-slate-700"
+          >Correo electrónico</span
+        >
+        <input
+          type="text"
+          class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        />
+      </label>
 
-          <DhbCardComponent 
-          :typeEvaluation='StatusCards.IncompleteEvaluation' 
-          :percent-one="{ percent:'20%'}" />
+      <label class="block my-5">
+        <span class="block text-left text-sm font-medium text-slate-700"
+          >Contraseña</span
+        >
+        <input
+          type="password"
+          class="mt-1 block w-full px-3 py-2 bg-white border border-slate-300 rounded-md text-sm shadow-sm placeholder-slate-400 focus:outline-none focus:border-green-500 focus:ring-1 focus:ring-sky-500 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 disabled:shadow-none invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500"
+        />
+      </label>
 
-          <DhbCardComponent 
-          :typeEvaluation='StatusCards.NoFitTheProfile' 
-          :percent-one="{ percent:'40%'}" />
-    </div>
-    <div class="grid grid-cols-1 gap-4  mx-4 lg:mx-0">
-        <div class="">
-          <DhbTableComponent />
-        </div>
+      <nuxtLink to="/dashboard">
+        <button
+          class="p-3 rounded-3xl w-full bg-gray-300 hover:bg-gray-400 active:bg-gray-500 focus:outline-none focus:ring focus:ring-gray-300"
+        >
+          Ingresar
+        </button>
+      </nuxtLink>
     </div>
   </div>
 </template>
